2c43ebcb48cd6c3ba39805244714f93becf12d3d
mohamed <zeafanm@gmail.com>
2018-05-01 06:16:55 +0200

    the Fisrt Push

---
+++ Resource_DictionaryProject/Resource_DictionaryProject/Resource_DictionaryProject/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Resource_DictionaryProject
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
